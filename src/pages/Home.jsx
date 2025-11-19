721dd94829e9460e5b1bc7a1c515ebc9913999c4
nacima <nassimajodar@gmail.com>
2025-11-19 11:17:36 +0100

    home page modification

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useEffect,useState } from 'react';
+
+export default function Home() {
+  return (
+    <div>
+        <div>
+            <img src="cinema-project/public/4b890462-e9e0-4532-84f4-6b4530699c14.webp" alt="" />
+            <h1>AUX JOURS QUI VIENNENT</h1>
+        </div>
+        <div>
+            <img src='cinema-project/public/5e331d5d-0d84-4009-808d-e22b9693497f.webp' alt=''/>
+            <h1>MARIUS</h1>
+        </div>
+        <div>
+            <img src="cinema-project/public/25ad5441-291a-4a9e-b7c6-e6d17d9b5e85.webp" alt=""/>
+            <h1>SALLY BAUER </h1>
+        </div>
+        <div>
+            <img src='cinema-project/public/43e14d99-6c24-45fe-9a07-3fc62b795f34.webp'/>
+
+        </div>
+
+
+    </div>
+  )
+}
